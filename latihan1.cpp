84312fac30b7db27f26b966b48cf35bc0631c96d
rafijadika <jadika.rafi@gmail.com>
2023-03-15 16:05:17 +0700

    first commit

---
+++ latihan1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int main (){
+    //numeric luas, P, L
+    double luas, P, L;
+    //Display, "masukan panjangnya : "
+    cout << " Masukan Panjangnya "
+    //Accept P
+    cin >> P;
+    //display, "Masukan Lebarnya : "
+    cout << "Masukan Lebarnya"
+    //Accept L
+    cn >> L;
+    //Compute Luas = P*L
+    luas = P*L:
+    //Display, " luasnya ; " + luas
+    cout << "Luasnya ; " << 
+}
